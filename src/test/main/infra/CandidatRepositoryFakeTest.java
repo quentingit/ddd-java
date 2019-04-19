b6ce16b1c2e55e98c5a9396ffa6684e59d4510f9
Nicolas Baptista <33665069+Kranysys@users.noreply.github.com>
2019-04-19 23:47:19 +0200

    CreneauTest testing on dates + CandidatRepositoryFake testing

--- src/test/main/infra/CandidatRepositoryFakeTest.java
+++ src/test/main/infra/CandidatRepositoryFakeTest.java
@@ -14,8 +14,7 @@ public class CandidatRepositoryFakeTest {
     public void getCandidatByID() {;
 
         //on recherche un candidat pour l'id 1
-         Candidat candidat =  new Candidat(1);
-         candidat= new CandidatRepositoryFake().getCandidatByID(1);
+         Candidat candidat = new CandidatRepositoryFake().getCandidatByID(1);
 
         //on creer un objet qui equivaut a candidat id=1
        //Candidat candidat= new Candidat(1, "jean", "IOS", 5);
@@ -46,6 +45,59 @@ public class CandidatRepositoryFakeTest {
         assertEquals(1, assertEquals );
     }
 
+    @Test
+    public void getCandidatByID2() {;
 
+        //on recherche un candidat pour l'id 7
+        Candidat candidat= new CandidatRepositoryFake().getCandidatByID(7);
 
+        //on creer un objet qui equivaut a candidat id=1
+        Candidat candidatEqual= new Candidat(1, "jean", "IOS", 5);
+
+        int assertEquals=0;
+
+        // comparing above created Objects.
+        if(candidatEqual.hashCode() == candidat.hashCode())
+        {
+            if(candidatEqual.equals(candidat)) {
+                System.out.println("Both Objects are equal. ");
+                assertEquals = 1;
+            }
+            else
+                System.out.println("Both Objects are not equal. ");
+
+        }
+        else
+            System.out.println("Both Objects are not equal. ");
+
+        assertEquals(0, assertEquals );
+    }
+
+    @Test
+    public void getCandidatByID3() {;
+
+        //on recherche un candidat pour l'id 7
+        Candidat candidat= new CandidatRepositoryFake().getCandidatByID(7);
+
+        //on creer un objet qui equivaut a candidat id=1
+        Candidat candidatEqual= new Candidat(7, "lucie", "Android", 6);
+
+        int assertEquals=0;
+
+        // comparing above created Objects.
+        if(candidatEqual.hashCode() == candidat.hashCode())
+        {
+            if(candidatEqual.equals(candidat)) {
+                System.out.println("Both Objects are equal. ");
+                assertEquals = 1;
+            }
+            else
+                System.out.println("Both Objects are not equal. ");
+
+        }
+        else
+            System.out.println("Both Objects are not equal. ");
+
+        assertEquals(1, assertEquals );
+    }
 }
---
+++ src/test/main/model/entretien/CandidatTest.java
@@ -0,0 +1,7 @@
+package main.model.entretien;
+
+import static org.junit.Assert.*;
+
+public class CandidatTest {
+
+}
